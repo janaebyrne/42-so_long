5ca9efed51e415b903294534a71773473fad3d76
janaebyrne <150924043+janaebyrne@users.noreply.github.com>
2024-09-05 14:42:01 +0200

    Update picture.c
    replace printf with ft_printf (need to add this to directory and makefile)

--- game_logic/picture.c
+++ game_logic/picture.c
@@ -62,23 +62,23 @@ void put_images(t_complete *game)
 
     game->background = mlx_xpm_file_to_image(game->mlx_ptr, "images/background.xpm", &i, &j);
     if (!game->background) 
-        { printf("Failed to load background image\n"); return; }
+        { ft_printf("Failed to load background image\n"); return; }
 
     game->wall = mlx_xpm_file_to_image(game->mlx_ptr, "images/wall.xpm", &i, &j);
     if (!game->wall) 
-        { printf("Failed to load wall image\n"); return; }
+        { ft_printf("Failed to load wall image\n"); return; }
    
     game->player = mlx_xpm_file_to_image(game->mlx_ptr, "images/turtle.xpm", &i, &j);
     if (!game->player)
-        { printf("Failed to load player image\n"); return; }
+        { ft_printf("Failed to load player image\n"); return; }
     
     game->exit = mlx_xpm_file_to_image(game->mlx_ptr, "images/treasure.xpm", &i, &j);
     if (!game->exit)
-        { printf("Failed to load exit image\n"); return; }
+        { ft_printf("Failed to load exit image\n"); return; }
     
     game->collectable = mlx_xpm_file_to_image(game->mlx_ptr, "images/fish.xpm", &i, &j);
     if (!game->collectable)
-        { printf("Failed to load collectable image\n"); return; }
+        { ft_printf("Failed to load collectable image\n"); return; }
 
 }
 
