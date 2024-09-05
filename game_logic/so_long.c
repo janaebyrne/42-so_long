4286670ba0ab0a7754d3964e9f93768bde2685b1
janaebyrne <150924043+janaebyrne@users.noreply.github.com>
2024-09-05 17:00:36 +0200

    Update so_long.c

--- game_logic/so_long.c
+++ game_logic/so_long.c
@@ -1,4 +1,3 @@
-
 #include "so_long.h"
 
 // #include <X11/keysym.h>  // maybe needed on linux later
@@ -12,10 +11,9 @@
 
 #define ESCAPE_KEY 53 //maybe change on linux later
 
-
 int on_destroy(t_complete *game)
 {
-	 printf("Destroying window and exiting...\n"); //for debuggging only - remove later
+	ft_printf("Destroying window and exiting...\n"); //for debuggging only - remove later
 	mlx_destroy_window(game->mlx_ptr, game->win_ptr);
 	//mlx_destroy_display(game->mlx_ptr);
 	// free(game->mlx_ptr);
@@ -27,7 +25,7 @@ int on_keypress(int keysym, t_complete *game)
 {
 	if (keysym == ESCAPE_KEY)
 	{
-		printf("Escape key pressed. Exiting...\n");
+		ft_printf("Escape key pressed. Exiting...\n");
 		//for debugging only - remove later
 		mlx_destroy_window(game->mlx_ptr, game->win_ptr);
 		//mlx_destroy_display(data->mlx_ptr);
@@ -69,7 +67,7 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		printf("usage: %s <map_file>\n", argv[0]);
+		ft_printf("usage: %s <map_file>\n", argv[0]);
 		return (1);
 	}
 
@@ -78,7 +76,7 @@ int	main(int argc, char **argv)
 
 	if (!read_map(&game, argv[1]))
 	{
-		printf("Failed to read map from file\n");
+		ft_printf("Failed to read map from file\n");
 		return(1);		
 	}
 
@@ -90,16 +88,16 @@ int	main(int argc, char **argv)
 	game.mlx_ptr = mlx_init();
 	if (!game.mlx_ptr)
 	{
-		printf("Failed to initialize MLX\n");//remove later
+		ft_printf("Failed to initialize MLX\n");//remove later
 		return (1);
 	}
 	game.win_ptr = mlx_new_window(game.mlx_ptr, window_width, window_height, "solong");
 	if (!game.win_ptr)
 	{
-		printf("Failed to create window\n");//remove later
+		ft_printf("Failed to create window\n");//remove later
 		return (1);
 	}
-	printf("Window created successfully\n");//remove later
+	ft_printf("Window created successfully\n");//remove later
 
 	// Load images
     put_images(&game); 
@@ -109,11 +107,11 @@ int	main(int argc, char **argv)
 
     // Register hooks
 	mlx_key_hook(game.win_ptr, controls_check, &game);
-    mlx_hook(game.win_ptr, 2, 1L << 0, &on_keypress, &game);
-    mlx_hook(game.win_ptr, 17, 0L, &on_destroy, &game);
+	mlx_hook(game.win_ptr, 2, 1L << 0, &on_keypress, &game);
+	mlx_hook(game.win_ptr, 17, 0L, &on_destroy, &game);
 
-    // Start the MLX loop
-    mlx_loop(game.mlx_ptr);
+    // Start the MLX loop 
+	mlx_loop(game.mlx_ptr);
 
 	// Free all allocated memory
 	free_map(game.map, game.mapheight);
